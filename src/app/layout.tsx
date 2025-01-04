b8b5d654f506ddb6ee5880bf90fe0a2d4f0675c4
HASURO <rentaro170928@icloud.com>
2025-01-04 17:14:34 +0900

    fix: metadata settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,32 @@
 import type { Metadata } from "next";
 import "./globals.scss";
 
+const title = "山上登記測量事務所";
+const description = "山上 博隆の登記測量事務所です。";
+const url = "https://yamauetakahiro.com";
+
 export const metadata: Metadata = {
-  title: "山上登記測量事務所",
-  description: "山上 博隆の登記測量事務所です。",
+  title: title,
+  description: description,
+  openGraph: {
+    title: {
+      default: title,
+      template: `%s - ${title}`,
+    },
+    description,
+    url,
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: {
+      default: title,
+      template: `%s - ${title}`,
+    },
+    description,
+    creator: '@HASURO_dev',
+  },
   verification: {
     google: "EaRzaToN8Od80w_p6Fg6xIL0s-PYYCiPNvTND1AZLkM",
   },
